7a333bcccc8a914536e44f1a5202522657458006
Maxime Giampaoli <maxime.giampaoli@gmail.com>
2025-05-19 09:59:40 +0200

    Algo 2 passed

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -11,8 +11,30 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
 function getFibonacciSequence(size: number): number[] {
-  // Ton code ici !
-  return [0, 1];
+  if (size <= 0) {
+    return [];
+  }
+
+  if (size === 1) {
+    return [0];
+  }
+
+  if (size === 2) {
+    return [0, 1];
+  }
+
+  const result = [0, 1];
+
+  for (let i = 2; i < size; i++) {
+    const lastNumber = result[result.length - 1];
+    const nextToLastNumber = result[result.length - 2];
+
+    const additionalFibonacci = lastNumber + nextToLastNumber;
+
+    result.push(additionalFibonacci);
+  }
+
+  return result;
 }
 
 export default getFibonacciSequence;
