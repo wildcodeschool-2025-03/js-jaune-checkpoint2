3cd52e6bc1ecf2ddf0a695ba013846b106f59499
GUILLAUME ZIMBERLIN <zimberlinguillaume@gmail.com>
2025-05-19 09:24:19 +0200

    Algo2

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -11,8 +11,26 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
 function getFibonacciSequence(size: number): number[] {
-  // Ton code ici !
-  return [0, 1];
+	// Ton code ici !
+
+	const fibonacci = [0, 1];
+
+	if (size <= 0) {
+		return [];
+	}
+	if (size === 1) {
+		return [0];
+	}
+	if (size === 2) {
+		return [0, 1];
+	}
+
+	for (let i = 2; i < size; i++) {
+		const valueToAdd = fibonacci[i - 1] + fibonacci[i - 2];
+		fibonacci.push(valueToAdd);
+	}
+
+	return fibonacci;
 }
 
 export default getFibonacciSequence;
