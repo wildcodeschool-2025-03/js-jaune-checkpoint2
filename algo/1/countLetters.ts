e68231318cf9a03c9eee150f2b52ff24fbb6f9dd
Maxime Giampaoli <maxime.giampaoli@gmail.com>
2025-05-19 09:28:41 +0200

    Algo 1 passed

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,16 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+  let counter = 0;
+  for (let i = 0; i < givenString.length; i++) {
+    const character = givenString[i];
+
+    if (character === letter) {
+      counter++;
+    }
+  }
+
+  return counter;
 }
 
 export default countLetters;
